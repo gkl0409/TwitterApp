89d38da852a83b73fa054125cf07e26ad9277b70
kaden <kaden@yahoo-inc.com>
2015-07-01 13:54:17 +0800

    Initial Commit

---
+++ TwitterAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitterAPP
+//
+//  Created by kaden Chiang on 2015/7/1.
+//  Copyright (c) 2015年 kaden Chiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwitterAPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwitterAPP
+//
+//  Created by kaden Chiang on 2015/7/1.
+//  Copyright (c) 2015年 kaden Chiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
